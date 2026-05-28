f0ab93e8c4e59bde215a1a501bde1dfa3ebaa3a4
EricBoittier <eric.boittier@icloud.com>
2026-05-28 12:51:45 +0200

    Update Slurm job configuration in README.md and jobscript.sh
    - Changed QoS setting from `gpu6hours` to `6hours` in both README.md and jobscript.sh.
    - Added note in README.md regarding partition-specific QoS requirements for SciCORE.

--- profiles/scicore/jobscript.sh
+++ profiles/scicore/jobscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=GPU_JOB
 #SBATCH --time=01:00:00
-#SBATCH --qos=gpu6hours
+#SBATCH --qos=6hours
 #SBATCH --mem-per-cpu=20G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
